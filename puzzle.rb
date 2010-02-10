8f22f073e1d28d2e72a95a180fcfe96688a26ebc
Brian Turnbull <github@brianturnbull.com>
2010-02-10 10:36:22 -0500

    Initial Commit

---
+++ puzzle.rb
@@ -0,0 +1,102 @@
+
+class Puzzle
+  attr_reader :tiles
+
+  class InvalidMove < Exception; end
+
+  def self.clone(puzzle)
+    new(puzzle.tiles)
+  end
+
+  ##
+  ## Represent the puzzle as N arrays of N length while addressing
+  ## individuals tiles by their x,y coordinates with 0,0 origin at
+  ## the upper left tile. The missing tile is represented by nil.
+  ##
+  ## For a 3x3 puzzle:
+  ## 
+  ##   [ [ 1, 2, 3   ],
+  ##     [ 4, 5, 6   ],
+  ##     [ 7, 8, nil ] ]
+  ##
+  ## Addressing a position with x,y coordinates requires reversing
+  ## the coordinates internally when using Ruby multi-dimensional
+  ## array syntax.  In the above example, the "6" at position 2,1
+  ## would be addressed as array[1][2].
+  ##
+  def initialize(arg = 3)
+    @tiles = arg.is_a?(Array) ? arg : generate_solved_array(arg)
+    @moves = 0
+  end
+
+  ##
+  ## Returns the x,y coordinates of the hole as an array
+  ##
+  def hole
+    @tiles.size.times do |x|
+      @tiles.size.times do |y|
+        return [x,y] if @tiles[y][x].nil?
+      end
+    end
+  end
+
+  ##
+  ## Return list of x,y coordinates for valid moves given the
+  ## current state of the puzzle
+  ##
+  def available_moves
+    adjacent_tiles(*self.hole)
+  end
+
+  ##
+  ## Pass the x,y coordinates of the tile to swap with the hole
+  ##
+  def move(dx, dy)
+    hole = self.hole
+    unless adjacent_tiles(dx, dy).include?(hole)
+      raise InvalidMove, "#{dx},#{dy} -> #{hole[0]},#{hole[1]}"
+    end
+    @tiles[dy][dx], @tiles[hole[1]][hole[0]] = @tiles[hole[1]][hole[0]], @tiles[dy][dx]
+    @moves += 1
+  end
+
+  def solved?
+    @tiles == generate_solved_array(@tiles.size)
+  end
+  
+  ##
+  ## Shuffle the puzzle.  Necessary since there are unsolvable
+  ## configurations of the tiles
+  ##
+  def shuffle(iterations = 5000)
+    iterations.times do
+      moves = available_moves
+      move(*moves[rand(moves.size)])
+    end
+    @moves = 0
+  end
+
+protected
+
+  ## create a solved puzzle with dim^2 tiles
+  def generate_solved_array(dim)
+    tiles = dim.times.map {|i| dim.times.map {|j| (j+1)+dim*(i)}}
+    tiles[dim-1][dim-1] = nil
+    tiles
+  end  
+
+  ## returns list of coordinates
+  def adjacent_tiles(dx, dy)
+    tiles = []
+    @tiles.size.times do |x|
+      @tiles.size.times do |y|
+        if ((x+1 == dx || x-1 == dx) && y == dy) || 
+           ((y+1 == dy || y-1 == dy) && x == dx)
+          tiles << [x, y]
+        end
+      end
+    end
+    tiles
+  end
+
+end
---
+++ spec/puzzle_spec.rb
@@ -0,0 +1,131 @@
+require File.join(File.dirname(__FILE__), '..', 'puzzle.rb')
+
+describe 'A default Puzzle' do
+
+  before :each do
+    @puzzle = Puzzle.new
+  end
+
+  it 'should have dimension of three' do
+    @puzzle.tiles.size.should == 3
+  end
+
+  it 'should be solved' do
+    @puzzle.solved?.should be_true
+  end
+
+  it 'should have the hole at 2,2' do
+    @puzzle.hole.should == [2,2]
+  end
+
+  it 'should allow valid moves' do
+    lambda {@puzzle.move(2,1)}.should_not raise_error
+  end
+
+  it 'should not allow invalid moves' do
+    lambda {@puzzle.move(1,1)}.should raise_error Puzzle::InvalidMove
+  end
+
+  it 'should list available moves' do
+    @puzzle.available_moves.should == [[1, 2], [2, 1]]
+  end
+
+  it 'should swap hole and tile after move' do
+    @puzzle.move(2,1)
+    @puzzle.hole.should == [2,1]
+  end
+
+  it 'should shuffle' do
+    @puzzle.shuffle(3)
+    @puzzle.should_not be_solved
+  end
+
+end
+
+describe 'A 16 tile Puzzle' do
+
+  before :each do
+    @puzzle = Puzzle.new(4)
+  end
+
+  it 'should accept dimension argument of 4' do
+    @puzzle.tiles.size.should == 4
+  end
+
+  it 'should be solved' do
+    @puzzle.should be_solved
+  end
+
+  it 'should have the hole at 3,3' do
+    @puzzle.hole.should == [3,3]
+  end
+
+  it 'should allow valid moves' do
+    lambda {@puzzle.move(3,2)}.should_not raise_error
+  end
+
+  it 'should not allow invalid moves' do
+    lambda {@puzzle.move(1,1)}.should raise_error Puzzle::InvalidMove
+  end
+
+  it 'should list available moves' do
+    @puzzle.available_moves.should == [[2, 3], [3, 2]]
+  end
+
+  it 'should swap hole and tile after move' do
+    @puzzle.move(3,2)
+    @puzzle.hole.should == [3,2]
+  end
+
+  it 'should shuffle' do
+    @puzzle.shuffle(3)
+    @puzzle.should_not be_solved
+  end
+
+end
+
+describe 'A specified Puzzle' do
+
+  before :each do
+    @puzzle = Puzzle.new([[1, 2], [3, nil]])
+  end
+
+  it 'should accept a 2 dimension array' do
+    @puzzle.tiles.size.should == 2
+  end
+
+  it 'should detect a solved puzzle' do
+    @puzzle.should be_solved
+  end
+
+  it 'should detect an unsolved puzzle' do
+    @puzzle = Puzzle.new([[nil, 1], [3, 2]])
+    @puzzle.should_not be_solved
+  end
+
+  it 'should detect the hole' do
+    @puzzle.hole.should == [1,1]
+  end
+
+  it 'should have the hole at 1,1' do
+    @puzzle.hole.should == [1,1]
+  end
+
+  it 'should allow valid moves' do
+    lambda {@puzzle.move(1,0)}.should_not raise_error
+  end
+
+  it 'should not allow invalid moves' do
+    lambda {@puzzle.move(0,0)}.should raise_error Puzzle::InvalidMove
+  end
+
+  it 'should list available moves' do
+    @puzzle.available_moves.should == [[0, 1], [1, 0]]
+  end
+
+  it 'should swap hole and tile after move' do
+    @puzzle.move(1,0)
+    @puzzle.hole.should == [1,0]
+  end
+
+end
